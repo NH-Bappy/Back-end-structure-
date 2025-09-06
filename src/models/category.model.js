327c48c15f08b2b8fe7c46e8659046691d3b2c9c
NH-Bappy <naimulbappy2207@gmail.com>
2025-09-06 13:50:43 +0600

    creating a slug

---
+++ src/models/category.model.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose
+const { apiResponse } = require("../utils/apiResponse");
+const { asyncHandler } = require("../utils/asyncHandler");
+const { CustomError } = require("../utils/customError");
+const { required, ref, boolean } = require('joi');
+const slugify = require('slugify')
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    image: {
+        type: String
+    },
+    slug: {
+        type: String
+    },
+    subCategory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: subCategory
+        }
+    ],
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+},
+    { timestamps: true }
+)
+
+// make a slug using name
+
+// Pre-save hook: runs before saving the document
+categorySchema.pre("save", function (next) {
+  // Only generate slug if 'name' was created/modified
+  if (this.isModified("name")) {
+    this.slug = slugify(this.name, {
+      replacement: "-",   // replace spaces with -
+      lower: true,        // convert to lowercase
+      strict: true,       // remove special characters
+      trim: true          // trim - from start & end
+    });
+  }
+
+  next(); // move to the next middleware
+});
+
+// check slug exist or not
+
+
+module.exports = mongoose.models.category || mongoose.model("category", categorySchema)
