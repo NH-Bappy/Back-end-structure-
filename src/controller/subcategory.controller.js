aa8b295ceab8a91cb6b670d74a2c4689750ba65e
NH-Bappy <naimulbappy2207@gmail.com>
2025-09-10 10:54:59 +0600

    find requested subcategory complete

--- src/controller/subcategory.controller.js
+++ src/controller/subcategory.controller.js
@@ -14,11 +14,20 @@ exports.createNewSubCategory = asyncHandler(async (req ,res) => {
     const subcategory = await subCategoryModel.create(value);
     if (!subcategory) throw new CustomError(401 , "please try again creating subcategory");
     apiResponse.sendSuccess(res , 200 , "Successfully created a new subcategory", subcategory);
-})
+});
 
-//@// show all the category
+//@desc show all the category
 exports.findAllSubcategories = asyncHandler(async(req ,res) => {
     const allSubcategory = await subCategoryModel.find().populate("category").sort({createdAt: -1})
     if(!allSubcategory) throw new CustomError(401 , "subcategory not found");
     apiResponse.sendSuccess(res , 200 ,"successfully found all the subcategory" , allSubcategory);
-})
+});
+
+//@desc find a specific category using slug
+exports.getSubcategoryBySlug = asyncHandler(async(req ,res) => {
+    const {slug} = req.params;
+    if(!slug) throw new CustomError(401 , " The slug does not match");
+    const specificSubcategory = await subCategoryModel.findOne({slug}).populate("category");
+    if(!specificSubcategory) throw new CustomError(401 , "Requested subcategory not found");
+    apiResponse.sendSuccess(res , 200 , "Requested subcategory found successfully" , specificSubcategory);
+});
