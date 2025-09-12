5d8563f5c2eec0ee9c6e16ae63457a4fa3175a59
NH-Bappy <naimulbappy2207@gmail.com>
2025-09-12 22:07:14 +0600

    find a specific brand complete

--- src/controller/brand.controller.js
+++ src/controller/brand.controller.js
@@ -10,7 +10,7 @@ const { validateBrandRequest } = require('../validation/brand.validation');
 exports.registerBrand = asyncHandler(async (req ,res) => {
     const value = await validateBrandRequest(req)
     // console.log(value);
-//@//@That line uploads the image file to Cloudinary and returns its hosted URL, so you can save the URL in your database instead of storing the file locally.
+//@That line uploads the image file to Cloudinary and returns its hosted URL, so you can save the URL in your database instead of storing the file locally.
     const uploadImageUrl = await uploadFileInCloudinary(value?.image?.path);
     if(!uploadImageUrl) throw new CustomError(500 , "your URL is missing or some wrong with your image or database");
 // Creates a new document in MongoDB using your brandModel, with the given value and image
@@ -19,9 +19,18 @@ exports.registerBrand = asyncHandler(async (req ,res) => {
     apiResponse.sendSuccess(res , 200 , "brand created successfully" , brand);
 });
 
-//@ find all brand 
+//@desc find all brand 
 exports.findAllBrand = asyncHandler(async(req ,res ) => {
     const brand = await brandModel.find({}).sort({createdAt: -1});
     if (!brand) throw new CustomError(401 , "all brand are not found");
     apiResponse.sendSuccess(res , 200 , "find all brand successfully" , brand);
-})
+});
+
+//@des find a specific brand using slug
+exports.getBrandBySlug = asyncHandler(async(req ,res) => {
+    const {slug} = req.params;
+    if(!slug) throw new CustomError(401 , " The slug does not match");
+    const brand = await brandModel.findOne({slug});
+    if(!brand) throw new CustomError(401 ,"Requested brand does not exist.");
+    apiResponse.sendSuccess(res , 200 , "Requested brand found successfully", brand);
+});
