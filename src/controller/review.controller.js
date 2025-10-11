f60f1c69fe9b2ab6e281d879c9542fcf243333eb
NH-Bappy <naimulbappy2207@gmail.com>
2025-10-11 23:30:55 +0600

    fetch: update product

--- src/controller/review.controller.js
+++ src/controller/review.controller.js
@@ -41,17 +41,30 @@ exports.createReview = asyncHandler(async (req, res) => {
 });
 
 //@ get all review 
-exports.getAllReview = asyncHandler(async(req ,res) => {
+exports.getAllReview = asyncHandler(async (req, res) => {
     const reviewObject = await reviewModel.find().sort({ createdAt: -1 });
-    if(!reviewObject) throw new CustomError(404 , "reviews not found");
-    apiResponse.sendSuccess(res , 200 ,"successfully founded all the review" ,reviewObject)
+    if (!reviewObject) throw new CustomError(404, "all reviews not found");
+    apiResponse.sendSuccess(res, 200, "successfully founded all the review", reviewObject)
 })
 
 //@ find single review 
 
+exports.getSingleReview = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    if (!id) throw new CustomError(404, "id is missing");
+    const reviewObject = await reviewModel.findOne({ _id: id });
+    if (!reviewObject) throw new CustomError(404, "single review not found");
+    apiResponse.sendSuccess(res, 200, "successfully found the review", reviewObject);
+});
 
-
-
+//@update review
+exports.reviewUpdate = asyncHandler(async(req ,res) => {
+    const {id} = req.params;
+    if (!id) throw new CustomError(404, "id is missing");
+    const updateReview = await reviewModel.findOneAndUpdate({_id: id} , {...req.body} ,{new:true});
+    if(!updateReview) throw new CustomError(304 , "review not modified")
+    apiResponse.sendSuccess(res , 200 ,"review modified successfully" ,updateReview)
+});
 
 
 
--- src/routes/api/review.api.js
+++ src/routes/api/review.api.js
@@ -5,7 +5,8 @@ const reviewController = require('../../controller/review.controller');
 
 _.route("/create-review").post(reviewController.createReview);
 _.route("/find-all-review").get(reviewController.getAllReview);
-
+_.route("/find-single-review/:id").get(reviewController.getSingleReview);
+_.route("/update-review/:id").put(reviewController.reviewUpdate);
 
 
 module.exports = _;
