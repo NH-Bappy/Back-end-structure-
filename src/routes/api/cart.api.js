f8ef4bf0fdf638079be3d3bd02382edb6646f293
NH-Bappy <naimulbappy2207@gmail.com>
2025-10-20 12:36:35 +0600

    fetch: remove item

--- src/routes/api/cart.api.js
+++ src/routes/api/cart.api.js
@@ -4,5 +4,6 @@ const cartController = require('../../controller/cart.controller');
 
 _.route("/add-to-cart").post(cartController.addToCart);
 _.route("/apply-coupon").post(cartController.applyCoupon);
+_.route("/remove-Item").put(cartController.removeItem);
 _.route("/clear-cart").delete(cartController.clearCart);
 module.exports = _;
