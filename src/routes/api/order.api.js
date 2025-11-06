62025751b1baf5bcaeca5c4624f99f86553d0204
NH-Bappy <naimulbappy2207@gmail.com>
2025-11-06 11:19:14 +0600

    fetch: order model

--- src/routes/api/order.api.js
+++ src/routes/api/order.api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const _ = express.Router();
+const orderController = require('../../controller/order.controller');
 
+
+_.route("/create-order")
 
 
 module.exports = _;
