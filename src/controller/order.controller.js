5cba6ace07c27a450e79b8893841176ed42f20a9
NH-Bappy <naimulbappy2207@gmail.com>
2025-11-08 21:58:28 +0600

    working on order

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -8,13 +8,25 @@ const orderModel = require('../models/order.mode');
 const productModel = require('../models/product.model');
 const variantModel = require('../models/variant.model');
 
-exports.createOrder = asyncHandler(async(req , res) => {
+exports.createOrder = asyncHandler(async (req, res) => {
     const { user, guestID, shippingInfo, deliveryCharge } = await validateOrder(req);
     // console.log(value)
     const Id = user ? { user } : { guestID }
     console.log(Id)
     const cart = await cartModel.findOne(Id);
+    if (!cart) throw new CustomError(404, "Cart not found");
     // console.log(cart);
 
+    // return
     // stock reduce
+
+
+    Promise.all(
+        cart.items.map((item) => {
+            if (item.product) {
+                productModel.findOneAndDelete({ _id: item.product }, { $inc: { item: -item.quantity } }
+                )
+            }
+        })
+    );
 });
