8de712301968b967e70c947ffb143808cb263243
NH-Bappy <naimulbappy2207@gmail.com>
2025-11-19 23:57:44 +0600

    setup axios

---
+++ src/controller/curier.controller.js
@@ -0,0 +1,10 @@
+const { apiResponse } = require('../utils/apiResponse');
+const { asyncHandler } = require('../utils/asyncHandler');
+const { CustomError } = require('../utils/customError');
+const orderModel = require('../models/order.model');
+
+
+
+
+
+
---
+++ src/helpers/axios.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+require('dotenv').config()
+
+
+
+
+
+const API = axios.create({
+    baseURL: `${process.env.BASE_URL}`,
+    timeout: 1000,
+    headers: {
+        'Api-Key': `${process.env.STEADFAST_API_KEY}`,
+        'Secret-Key': ` ${process.env.STEADFAST_SECRET_KEY}`,
+        "Content-Type": "application/json"
+    }
+});
+
+module.exports = { API }
