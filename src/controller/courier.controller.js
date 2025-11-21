6cc19fcf3b2fb343be83ea4f2d4b8a0ddd341ca1
NH-Bappy <naimulbappy2207@gmail.com>
2025-11-21 20:42:55 +0600

    add new function

--- src/controller/courier.controller.js
+++ src/controller/courier.controller.js
@@ -2,6 +2,7 @@ const { apiResponse } = require('../utils/apiResponse');
 const { asyncHandler } = require('../utils/asyncHandler');
 const { CustomError } = require('../utils/customError');
 const orderModel = require('../models/order.model');
+const { default: mongoose } = require('mongoose');
 
 
 
@@ -10,5 +11,10 @@ const orderModel = require('../models/order.model');
 
 exports.createCourier = asyncHandler(async(req ,res) => {
     const { courierId } = req.body;
-    console.log(courierId)
+    if (!mongoose.Types.ObjectId.isValid(courierId)){
+        throw new CustomError(401 , "courier order not found");
+    }
+    const orderObject = await orderModel.findById(courierId)
+    if(!orderObject) throw new CustomError(404 , "order object not found")
+    console.log(orderObject)
 });
